294df463797409cc7414976b2fde3624201215fe
Omar Alberto Torres <51687480+hombremono873@users.noreply.github.com>
2024-10-01 10:53:50 -0500

    Update FabricaescuelaApplication.java

--- FabricaEscuela/src/main/java/fabricaescuela/fabricaescuela/FabricaescuelaApplication.java
+++ FabricaEscuela/src/main/java/fabricaescuela/fabricaescuela/FabricaescuelaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FabricaescuelaApplication {
 
 	public static void main(String[] args) {
+		String var = "Hola mundo";
 		SpringApplication.run(FabricaescuelaApplication.class, args);
 	}
 
